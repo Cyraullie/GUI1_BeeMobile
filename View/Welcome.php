5205ea29fa14977e1a1570b503bed247ffc59148
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-10-11 11:13:33 +0200

    Finish WelcomePage

--- View/Welcome.php
+++ View/Welcome.php
@@ -2,9 +2,15 @@
 ob_start();
 ?>
 
-<section class="Background Welcome">
- sdgsfh
-</section>
+<div class="background_welcome">
+
+    <a id="title">BeeMobile</a>
+    <div id="welcome">
+        <p>Vous avez déjà un compte ?</p>
+        <a class="button" href="?action=Login" >Se connecter</a>
+        <a href="?action=Register">Sinon inscrivez-vous en cliquant ici</a>
+    </div>
+</div>
 
 
 <?php
