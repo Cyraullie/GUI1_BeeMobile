4f01748f5449c7e24dba8d7d337fb50b423ace12
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-10-11 11:13:39 +0200

    Add routeur

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
+$action = null;
 
-require "View/Welcome.php";
+if (isset($_GET['action'])){
+    $action = $_GET['action'];
+}
+
+switch ($action) {
+    case "Login" :
+        require "View/Login.php";
+        break;
+    case "Register" :
+        require "View/Register.php";
+        break;
+    case "Home" :
+        require "View/Home.php";
+        break;
+
+
+
+
+    default :
+        require "View/Welcome.php";
+        break;
+}
+
+
+
