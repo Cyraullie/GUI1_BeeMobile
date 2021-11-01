b9e1f445d57440fe4a6ded03c801093642d02e37
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-01 15:49:39 +0100

    Add swiper for calendar day

--- View/CalendarDay.php
+++ View/CalendarDay.php
@@ -1,128 +1,191 @@
 <?php
 ob_start();
+
 ?>
 
-    <div class="background">
-        <div class="calendar_button">
-            <a id="day" href="?action=AgendaDay" class="active">Jour</a>
-            <a id="week" href="?action=AgendaWeek" class="inactive">Semaine</a>
-            <a id="month" href="?action=AgendaMonth" class="inactive">Mois</a>
+            <div class="background">
+                <div class="calendar_button">
+                    <a id="day" href="?action=AgendaDay" class="active">Jour</a>
+                    <a id="week" href="?action=AgendaWeek" class="inactive">Semaine</a>
+                    <a id="month" href="?action=AgendaMonth" class="inactive">Mois</a>
+                </div>
+                <!-- Slider main container -->
+                <div class="swiper">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                <?php for ($i = 0; $i < 7; $i++): ?>
+                    <div class="swiper-slide">
+                        <h1 class="title"><?= date("d/m/Y", strtotime("+ {$i} day")) ?></h1>
+                        <div class="background_calendar">
+
+                            <div class="title_day">
+                                <a>10h00</a>
+                            </div>
+                            <div class="area_parent">
+                                <div class="content">
+
+                                    <a class="area_child">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a
+                                        tellus pretium
+                                        dictum.
+                                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac
+                                        fermentum diam, ut
+                                        suscipit
+                                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur
+                                        efficitur nec
+                                        tortor
+                                        sed
+                                        imperdiet.
+
+                                    </a>
+                                </div>
+
+                                <div class="content">
+
+                                    <a class="area_child">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a
+                                        tellus pretium
+                                        dictum.
+                                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac
+                                        fermentum diam, ut
+                                        suscipit
+                                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur
+                                        efficitur nec
+                                        tortor
+                                        sed
+                                        imperdiet.
+
+                                    </a>
+                                </div>
+                            </div>
+
+
+                            <div class="title_day">
+                                <a>12h30</a>
+                            </div>
+                            <div class="area_parent">
+                                <div class="content">
+
+                                    <a class="area_child">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a
+                                        tellus pretium
+                                        dictum.
+                                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac
+                                        fermentum diam, ut
+                                        suscipit
+                                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur
+                                        efficitur nec
+                                        tortor
+                                        sed
+                                        imperdiet.
+
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="title_day">
+                                <a>15h30</a>
+                            </div>
+                            <div class="area_parent">
+                                <div class="content">
+
+                                    <a class="area_child">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a
+                                        tellus pretium
+                                        dictum.
+                                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac
+                                        fermentum diam, ut
+                                        suscipit
+                                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur
+                                        efficitur nec
+                                        tortor
+                                        sed
+                                        imperdiet.
+
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="title_day">
+                                <a>16h30</a>
+                            </div>
+                            <div class="area_parent">
+                                <div class="content">
+
+                                    <a class="area_child">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a
+                                        tellus pretium
+                                        dictum.
+                                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac
+                                        fermentum diam, ut
+                                        suscipit
+                                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur
+                                        efficitur nec
+                                        tortor
+                                        sed
+                                        imperdiet.
+
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="title_day">
+                                <a>18h30</a>
+                            </div>
+                            <div class="area_parent">
+                                <div class="content">
+
+                                    <a class="area_child">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a
+                                        tellus pretium
+                                        dictum.
+                                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac
+                                        fermentum diam, ut
+                                        suscipit
+                                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur
+                                        efficitur nec
+                                        tortor
+                                        sed
+                                        imperdiet.
+
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                <?php endfor; ?>
+
+            </div>
         </div>
-
-        <h1 class="title"><?= date("d/m/Y"); ?></h1>
-        <div class="background_calendar">
-
-
-            <div class="title_day">
-                <a>10h00</a>
-            </div>
-            <div class="area_parent">
-                <div class="content">
-
-                    <a class="area_child">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a tellus pretium
-                        dictum.
-                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac fermentum diam, ut
-                        suscipit
-                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur efficitur nec tortor
-                        sed
-                        imperdiet.
-
-                    </a>
-                </div>
-
-                <div class="content">
-
-                    <a class="area_child">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a tellus pretium
-                        dictum.
-                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac fermentum diam, ut
-                        suscipit
-                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur efficitur nec tortor
-                        sed
-                        imperdiet.
-
-                    </a>
-                </div>
             </div>
 
-            <div class="title_day">
-                <a>12h30</a>
-            </div>
-            <div class="area_parent">
-                <div class="content">
 
-                    <a class="area_child">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a tellus pretium
-                        dictum.
-                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac fermentum diam, ut
-                        suscipit
-                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur efficitur nec tortor
-                        sed
-                        imperdiet.
+    <script type="module">
 
-                    </a>
-                </div>
-            </div>
+        const swiper = new Swiper('.swiper', {
+            // Optional parameters
+            direction: 'horizontal',
 
-            <div class="title_day">
-                <a>15h30</a>
-            </div>
-            <div class="area_parent">
-                <div class="content">
 
-                    <a class="area_child">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a tellus pretium
-                        dictum.
-                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac fermentum diam, ut
-                        suscipit
-                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur efficitur nec tortor
-                        sed
-                        imperdiet.
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
 
-                    </a>
-                </div>
-            </div>
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
 
-            <div class="title_day">
-                <a>16h30</a>
-            </div>
-            <div class="area_parent">
-                <div class="content">
-
-                    <a class="area_child">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a tellus pretium
-                        dictum.
-                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac fermentum diam, ut
-                        suscipit
-                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur efficitur nec tortor
-                        sed
-                        imperdiet.
-
-                    </a>
-                </div>
-            </div>
-
-            <div class="title_day">
-                <a>18h30</a>
-            </div>
-            <div class="area_parent">
-                <div class="content">
-
-                    <a class="area_child">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a tellus pretium
-                        dictum.
-                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac fermentum diam, ut
-                        suscipit
-                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur efficitur nec tortor
-                        sed
-                        imperdiet.
-
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        });
+    </script>
 
 
 <?php
--- View/Layout.php
+++ View/Layout.php
@@ -22,6 +22,10 @@
     <!-- JavaScript Bundle with Popper -->
     <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
 </head>
 <body>
 
