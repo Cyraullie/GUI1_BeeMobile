3582efc3cd5b5b0735f23d72f00fd9e88270573f
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-01 08:20:12 +0100

    Finish style of home page

--- View/Home.php
+++ View/Home.php
@@ -2,17 +2,44 @@
 ob_start();
 ?>
 
-<div class="background">
-    <div id="login">
-        <a id="title_form">Connexion</a>
-        <form method="get">
-            <input type="hidden" name="action" value="home">
-            <input class="input_text" placeholder="Nom d'utilisateur" type="text">
-            <input class="input_text" placeholder="Mot de passe" type="password">
-            <input class="input_submit" type="submit" value="Connexion">
-        </form>
+    <div class="background">
+
+        <div class="area_parent">
+            <div class="title">
+                <a>Rappel</a>
+            </div>
+
+            <div class="area_child">
+                <div class="title">
+                    <a>10h00</a>
+                </div>
+                <div class="content">
+                    <a>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida nibh a tellus pretium
+                        dictum.
+                        Ut sit amet purus dapibus, pulvinar mauris sed, molestie arcu. Proin ac fermentum diam, ut
+                        suscipit
+                        libero. Nam ut ipsum ut justo dignissim egestas nec nec neque. Curabitur efficitur nec tortor
+                        sed
+                        imperdiet.
+
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="area_parent">
+            <div class="title">
+                <a>Notes</a>
+            </div>
+            <div class="content">
+                    <a class="area_child">faires des trucs pas bien</a>
+                    <a class="area_child">faires des trucs vraiments pas bien</a>
+            </div>
+        </div>
+
     </div>
-</div>
 
 
 <?php
