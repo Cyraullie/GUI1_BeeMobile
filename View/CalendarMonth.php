2eac6a26305578acafe515f19483877b706d3a1b
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-05 15:02:12 +0100

    Add slider

--- View/CalendarMonth.php
+++ View/CalendarMonth.php
@@ -3,7 +3,7 @@ ob_start();
 ?>
 <?php
 
-$z = 0;
+$z = 1;
 ?>
     <div class="background">
         <div class="calendar_button">
@@ -11,35 +11,49 @@ $z = 0;
             <a id="week" href="?action=AgendaWeek" class="inactive">Semaine</a>
             <a id="month" href="?action=AgendaMonth" class="active">Mois</a>
         </div>
+        <!-- Slider main container -->
+        <div class="swiper">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
 
+                    <?php for ($x = 0; $x <= 11; $x++): ?>
 
-        <h1 class="title"><?= date("F"); ?></h1>
-        <div class="background_calendar">
+                    <div class="swiper-slide">
+                        <div class="background_calendar">
+                        <h1 class="title"><?= date("F", strtotime("+ {$x} month")); ?></h1>
+                        <table>
+                            <tr>
+                                <th> lun</th>
+                                <th> mar</th>
+                                <th> mer</th>
+                                <th> jeu</th>
+                                <th> ven</th>
+                                <th> sam</th>
+                                <th> dim</th>
+                            </tr>
 
-            <table>
-                <tr>
-                    <th> lun</th>
-                    <th> mar</th>
-                    <th> mer</th>
-                    <th> jeu</th>
-                    <th> ven</th>
-                    <th> sam</th>
-                    <th> dim</th>
-                </tr>
-                <?php for ($y = 0; $y < (date('t') / 7); $y++): ?>
-                    <tr>
-                        <?php for ($i = 0; $i < 7; $i++): ?>
-                            <?php if ($z == date("t")): break; endif; ?>
-                            <td class="month">
-                                <div class="title_day"></div>
-                                <div class="area_parent <?= date("d")==$z+1 ? "thisDay" : "" ?>">
-                                        <h1 class="dayOfMonth"><?= date('d', strtotime("+ {$z} day")); ?></h1>
-                                </div>
-                            </td>
-                            <?php $z++; endfor ?>
-                    </tr>
-                <?php endfor ?>
-            </table>
+                            <?php for ($y = 0; $y < (date('t', strtotime("+ {$x} month")) / 7); $y++): ?>
+                                <tr>
+                                    <?php for ($i = 0; $i < 7; $i++): ?>
+
+                                        <td class="month">
+                                            <div class="title_day"></div>
+                                            <div class="area_parent <?= date("d") == $z ? "thisDay" : "" ?>">
+                                                <h1 class="dayOfMonth"><?= ($z <= 9) ? "0" . $z : $z ?></h1>
+                                            </div>
+                                        </td>
+                                        <?php if ($z == date("t", strtotime("+ {$x} month"))): $z = 1;
+                                            break; endif; ?>
+                                        <?php $z++; endfor ?>
+                                </tr>
+                            <?php endfor ?>
+                        </table>
+                        </div>
+                    </div>
+                    <?php endfor ?>
+
+            </div>
         </div>
     </div>
 
