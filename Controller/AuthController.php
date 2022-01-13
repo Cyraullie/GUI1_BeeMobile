0a3c7821ec79be70dc44560715ca1a3a03ee2521
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-01-13 16:19:33 +0100

    Add check register to insert a user in the db

--- Controller/AuthController.php
+++ Controller/AuthController.php
@@ -10,26 +10,48 @@ class AuthController
         $this->collection = $DB->ConnectDB();
     }
 
-    function indexLogin(){
+    function indexLogin()
+    {
         require "View/Login.php";
     }
 
-    function checkLogin(){
-        $user = $this->collection->find(['name'=>$_POST['username']]);
+    function checkLogin()
+    {
+        $user = $this->collection->find(['name' => $_POST['username']]);
 
-        foreach ($user as $client){
-            if($_POST['password'] != $client['password']){
+        foreach ($user as $client) {
+            if ($_POST['password'] != $client['password']) {
                 require "View/Login.php";
-            }else{
+            } else {
                 $_SESSION['user'] = $_POST['username'];
                 require "View/Home.php";
                 break;
             }
         }
-
-
-
+        require "View/Login.php";
     }
 
+    function indexRegister()
+    {
+        require "View/Register.php";
+    }
 
+    function checkRegister()
+    {
+        $user = $this->collection->find(['name' => $_POST['username']]);
+
+        foreach ($user as $client) {
+            if ($client['name'] != null) {
+                require "View/Login.php";
+            }
+        }
+
+        if ($_POST['password'] == $_POST['confirm_password']) {
+            $this->collection->insertOne(['name' => $_POST['username'], 'password' => $_POST['password']]);
+            $_SESSION['user'] = $_POST['username'];
+            require "View/Home.php";
+        } else {
+            require "View/Register.php";
+        }
+    }
 }
