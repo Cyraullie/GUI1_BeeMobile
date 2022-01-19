2c2c4ecd3ef7c219795a140a0eb73d7f5427fc8c
Cyril Goldenschue <cyril.goldenschue@cpnv.ch>
2022-01-19 11:23:30 +0100

    Add code to get the stats selected

--- Controller/StatsController.php
+++ Controller/StatsController.php
@@ -18,6 +18,17 @@ class StatsController{
 
 
     function edit(){
+        $user = $this->collection->findOne(['name'=>$_SESSION['user']]);
+        for ($i = 0; $i < count($user['hives']); $i++){
+
+            if($user['hives'][$i]['name'] == substr($_GET['hive'], 1, -1)) {
+                $hive = $user['hives'][$i];
+                for ($x = 0; $x < count($hive["stats"]); $x++)
+                if ($hive["stats"][$i]['date'] == substr($_GET['stat'], 1, -1)){
+                    $stats = $hive["stats"][$i];
+                }
+            }
+        }
 
         require "View/EditStat.php";
     }
