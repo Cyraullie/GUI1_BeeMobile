70e91c22f472494debef6ce1051bed2a0999ac94
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-01-20 15:15:09 +0100

    Create HiveController

---
+++ Controller/HiveController.php
@@ -0,0 +1,54 @@
+<?php
+
+class HiveController
+{
+
+    private $db;
+
+    public function __construct()
+    {
+
+        $NewDb = new DatabaseManager();
+        $this->db = $NewDb->ConnectDB();
+    }
+
+    function create()
+    {
+        require "View/CreateHive.php";
+    }
+
+    function save()
+    {
+        $user = $this->db->users->findOne(['name' => $_SESSION['user']]);
+        $hives = $this->db->hives->find(['userid' => $user->id]);
+        foreach ($hives as $hive) {
+            if ($hive['name'] == $_POST['hiveName']) {
+                $message = "Vous avez déjà une ruche avec ce nom";
+                $type_message = "error";
+                require "View/CreateHive.php";
+            }
+        }
+        $hives = $this->db->hives->find();
+        foreach ($hives as $hive) {
+            $id++;
+        }
+
+        $this->db->hives->insertOne(['userid' => intval($user->userid), 'hiveid' => $id, 'name' => $_POST['hiveName']]);
+
+        header('Location: ?action=Stats');
+        /*require_once "Controller/StatsController.php";
+        $StatsController = new StatsController();
+        $StatsController->index();*/
+
+    }
+
+    function edit()
+    {
+
+    }
+
+    function update()
+    {
+       
+    }
+}
