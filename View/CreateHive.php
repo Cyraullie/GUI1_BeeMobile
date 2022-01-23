c147a6d1eac19f345db44ff03252df3ebf5d7233
Cyril Goldenschue <cyril.goldenschue@cpnv.ch>
2022-01-23 12:53:29 +0100

    Delete useless input

--- View/CreateHive.php
+++ View/CreateHive.php
@@ -12,8 +12,6 @@ ob_start();
                     <a class="form title_day_hive"></a>
                     <div class="form hive_area">
                         <form action="?action=SaveHive" method="post">
-                            <input type="hidden" name="hive" value="<?= $stats['hiveid'] ?>">
-                            <input type="hidden" name="stat" value="<?= $stats['_id'] ?>">
                             <div class="stat_form_size">
                                 <div class="title_note_hive">
                                     <label for="hiveName">Nom de la ruche</label>
@@ -21,7 +19,7 @@ ob_start();
                                 <input id="hiveName" name="hiveName" class="area_child hive content form" type="text">
 
                             </div>
-                            <input class="button" value="Mettre à jour" type="submit">
+                            <input class="button" value="Créer" type="submit">
                         </form>
                     </div>
                 </div>
